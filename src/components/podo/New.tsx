83dc29a66d4f3daf396590a19c6fea97fa293e89
hakyung <diveindeep.dev@gmail.com>
2024-02-02 19:51:28 +0900

    Add style of NewPodo

--- src/components/podo/New.tsx
+++ src/components/podo/New.tsx
@@ -2,7 +2,106 @@ import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import useForm from '../../hooks/useForm';
 import { AxiosError } from 'axios';
 import { addPodoApi } from '../../features/podo/api';
+import { LuGrape } from 'react-icons/lu';
+import { IoIosPricetags, IoMdTrophy } from 'react-icons/io';
 import { toast } from '../toast';
+import styled from 'styled-components';
+import { ButtonStyle, ContentContainer, flexCenter } from '../../styles/Common';
+import { COLOR } from '../../styles/Variables';
+
+const Icon = styled.div`
+  ${flexCenter}
+  width: 50px;
+  height: 50px;
+  margin-right: 15px;
+  color: ${COLOR.bg};
+  font-size: 1.7rem;
+  background-color: ${COLOR.primary};
+  border-radius: 50%;
+`;
+
+const Label = styled.label`
+  display: flex;
+  align-items: center;
+  padding: 5px 0;
+  color: ${COLOR.textLight};
+
+  svg {
+    margin-right: 10px;
+    font-size: 1.5rem;
+  }
+
+  &:focus-within {
+    color: ${COLOR.primary};
+    font-weight: 500;
+  }
+
+  input {
+    margin: 5px 0;
+    padding: 5px 0;
+    width: 100%;
+    font-size: 0.9rem;
+    border-bottom: 1px solid ${COLOR.textLight};
+
+    &::placeholder {
+      color: ${COLOR.textLight};
+    }
+    &:focus {
+      border-bottom: 1px solid ${COLOR.primary};
+    }
+  }
+
+  &.goal {
+    margin: 0 0 10px 0;
+    padding: 0;
+    height: 50px;
+
+    input {
+      margin: 0;
+      padding: 0;
+      color: ${COLOR.primary};
+      font-size: 1.8rem;
+      font-weight: 700;
+      border-bottom: 0;
+    }
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  div {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+  }
+`;
+
+const Div = styled.div`
+  ${ContentContainer}
+
+  form {
+    display: flex;
+    flex-direction: column;
+    button {
+      ${ButtonStyle}
+      align-self: flex-end;
+      width: 170px;
+      padding: 15px;
+      margin-top: 20px;
+      color: ${COLOR.bg};
+      background-color: ${COLOR.primary};
+      font-weight: 500;
+      border: 1px solid ${COLOR.primary};
+
+      &:disabled {
+        cursor: initial;
+        color: ${COLOR.textLight};
+        background-color: ${COLOR.bgSub};
+        border: 1px solid ${COLOR.textLight};
+      }
+    }
+  }
+`;
 
 const initialValues: PodoForm = {
   title: '',
@@ -58,46 +157,51 @@ function NewPodo() {
   };
 
   return (
-    <div>
-      <h3>새로운 포도</h3>
+    <Div>
       <form onSubmit={handleSubmit}>
-        <label>
-          목표
-          <input
-            type="text"
-            name="title"
-            value={values.title}
-            placeholder="어떤 포도입니까?"
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          보상
-          <input
-            type="text"
-            name="reward"
-            value={values.reward}
-            placeholder="마지막 포도알을 달성하면?"
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          태그
-          <input
-            type="text"
-            name="reward"
-            value={tagString}
-            placeholder="콤마(,)로 구분합니다."
-            onChange={handleChangeTag}
-          />
-        </label>
-        <div>
-          <button type="submit" disabled={disabled}>
-            시작
-          </button>
-        </div>
+        <Wrapper>
+          <label>
+            <Icon>
+              <LuGrape />
+            </Icon>
+          </label>
+          <div>
+            <Label className="goal">
+              <input
+                type="text"
+                name="title"
+                value={values.title}
+                placeholder="목표: 어떤 포도입니까?"
+                onChange={handleChange}
+              />
+            </Label>
+            <Label>
+              <IoMdTrophy />
+              <input
+                type="text"
+                name="reward"
+                value={values.reward}
+                placeholder="마지막 포도알을 달성하면?"
+                onChange={handleChange}
+              />
+            </Label>
+            <Label>
+              <IoIosPricetags />
+              <input
+                type="text"
+                name="reward"
+                value={tagString}
+                placeholder="콤마(,)로 구분합니다."
+                onChange={handleChangeTag}
+              />
+            </Label>
+          </div>
+        </Wrapper>
+        <button type="submit" disabled={disabled}>
+          등록
+        </button>
       </form>
-    </div>
+    </Div>
   );
 }
 
