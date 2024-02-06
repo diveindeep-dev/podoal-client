f5ed6d522baa22a1aa19001c7473d5ab3df639d0
hakyung <diveindeep.dev@gmail.com>
2024-02-06 15:30:44 +0900

    Implement infinite scroll with Intersection Observer API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,13 +8,17 @@ import { toast } from '../components/toast';
 function Index() {
   const [list, setList] = useState<Podo[]>([]);
   const [page, setPage] = useState<number>(1);
+  const [target, setTarget] = useState<HTMLDivElement | null>(null);
 
   const getList = async () => {
     try {
       const response = await getPodoListByPageApi(page);
       if (response) {
         if (response.status === 200) {
-          setList(response.data.podos);
+          if (target && response.data.podos.length < 5) {
+            setTarget(null);
+          }
+          setList((prev) => [...prev, ...response.data.podos]);
         }
       }
     } catch (error) {
@@ -27,15 +31,35 @@ function Index() {
     }
   };
 
+  const onIntersect: IntersectionObserverCallback = (entreis) => {
+    entreis.forEach((entry) => {
+      if (entry.isIntersecting) {
+        setPage((prev) => prev + 1);
+      }
+    });
+  };
+
+  useEffect(() => {
+    let observer: IntersectionObserver;
+
+    if (target) {
+      observer = new IntersectionObserver(onIntersect, { threshold: 0.1 });
+      observer.observe(target);
+    }
+
+    return () => observer && observer.disconnect();
+  }, [target]);
+
   useEffect(() => {
     getList();
-  }, []);
+  }, [page]);
 
   return (
     <div>
       <h1>Podoal</h1>
       <NewPodo />
       <PodoList list={list} />
+      <div ref={setTarget}>You're up to date.</div>
     </div>
   );
 }
